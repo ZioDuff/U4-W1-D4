2f7fd8335570b7d571c990a0b3fbbc46b4d029ec
Jacopo De maio <jajodemaio@gmail.com>
2024-05-23 18:04:15 +0200

    small fix

--- src/Entities/Volontario.java
+++ src/Entities/Volontario.java
@@ -16,6 +16,32 @@ public class Volontario implements CheckIn {
     }
 
     //    METODI
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCV() {
+        return CV;
+    }
+
+    public void setCV(String CV) {
+        this.CV = CV;
+    }
+
     @Override
     public void checkIn() {
         System.out.println("oggi è proprio una bella giornata per lavorare 18 ore");
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import Intefaces.CheckIn;
 
 public class Main {
     public static void main(String[] args) {
+//        (-----------------------------EX1--------------------------)
 //        Dipendente jacopo = new Dipendente("1", 2000.15, TypeOfWork.AMMINISTRAZIONE);
 //        Dipendente giovanni = new Dipendente("2", 1999, TypeOfWork.VENDITE);
 //        Dipendente davide = new Dipendente("3", 1999, TypeOfWork.PRODUZIONE);
@@ -14,7 +15,7 @@ public class Main {
 //        for (int i = 0; i < arrDipendenti.length; i++) {
 //            System.out.println("Matricola Numero : " + arrDipendenti[i].get_id());
 //        }
-
+//        (----------------------------EX2-----------------------------------)
         DipendenteFullTIme jacopo = new DipendenteFullTIme("1", 5.6, TypeOfWork.AMMINISTRAZIONE);
 
         DipendentePartTIme giovanni = new DipendentePartTIme("12", 2, TypeOfWork.PRODUZIONE);
@@ -22,14 +23,12 @@ public class Main {
         Dirigente gesu = new Dirigente("0", 9999, TypeOfWork.VENDITE);
 
         Dipendente[] arrDipendenti = {jacopo, giovanni, gesu};
-//        System.out.println("Stipendio 28 giorni di jacopo: " + jacopo.calculateSalary(28) + " euri");
-//        System.out.println("Stipendio 28 giorni di giovanni: " + giovanni.calculateSalary(28) + " euri");
-//        System.out.println("Stipendio 28 giorni di gesu: " + gesu.calculateSalary(28) + " euri");
-//        System.out.println("Questo è il totale degli stipendi se hanno lavorato 28 giorni: " + (jacopo.calculateSalary(28) + giovanni.calculateSalary(28) + gesu.calculateSalary(28)) + " euri");
+        double sum = 0;
         for (int i = 0; i < arrDipendenti.length; i++) {
-            System.out.println("questo è lo stipendio della matricola " + arrDipendenti[i].get_id() + " ed è " + arrDipendenti[i].calculateSalary(28));
+            sum += arrDipendenti[i].calculateSalary(28);
         }
-
+        System.out.println("la somma degli stipendi è " + sum);
+//        (-------------------------------EX3------------------------)
         Volontario noe = new Volontario("noe", 99);
 
         CheckIn[] tuttiALavorare = {jacopo, noe, giovanni, gesu};
